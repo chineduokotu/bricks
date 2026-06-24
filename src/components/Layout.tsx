b7f968c88d083a3394ceede0d5f2725eefc04fc5
chineduokotu <chineokotu@gmail.com>
2026-06-24 01:23:54 +0100

    fixed top screw

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,5 @@
-import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 import { Box } from '@mui/material';
 import Navbar from './Navbar';
 import Footer from './Footer';
@@ -6,9 +7,16 @@ import CartDrawer from './CartDrawer';
 import WhatsAppFloat from './WhatsAppFloat';
 import CookieConsent from './CookieConsent';
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => window.scrollTo(0, 0), [pathname]);
+  return null;
+}
+
 export default function Layout() {
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+      <ScrollToTop />
       <Navbar />
       <Box component="main" sx={{ flex: 1 }}>
         <Outlet />
